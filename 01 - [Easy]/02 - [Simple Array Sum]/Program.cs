b0d973accef67ec3653aa456256fe3953fc50922
Georgi <alienguymc@gmail.com>
2020-09-23 21:46:49 +0300

    add

---
+++ 01 - [Easy]/02 - [Simple Array Sum]/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numsCount = int.Parse(Console.ReadLine());
+
+            var arr = new int[numsCount];
+
+            var nums = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            AddingNumbers(arr, nums);
+
+            Console.WriteLine(arr.Sum());
+        }
+
+        private static void AddingNumbers(int[] arr, int[] nums)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = nums[i];
+            }
+        }
+    }
+}
