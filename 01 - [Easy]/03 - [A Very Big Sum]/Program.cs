7c32650b8321d142784cd81d141f619561b7250e
Georgi <alienguymc@gmail.com>
2020-09-23 22:05:40 +0300

    add

---
+++ 01 - [Easy]/03 - [A Very Big Sum]/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace AVeryBigSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elementsCount = int.Parse(Console.ReadLine());
+            BigInteger[] numbers = new BigInteger[elementsCount];
+
+            var nums = Console
+                .ReadLine()
+                .Split()
+                .Select(BigInteger.Parse)
+                .ToArray();
+
+            BigInteger sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = nums[i];
+                sum += numbers[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
