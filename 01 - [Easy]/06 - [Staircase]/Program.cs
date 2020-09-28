24b9a45fb508ab581f5e5b9274cb925dd11bc478
Georgi <alienguymc@gmail.com>
2020-09-28 21:58:53 +0300

    add

---
+++ 01 - [Easy]/06 - [Staircase]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i > 0; i--)
+            {
+                Console.WriteLine(new string(' ', i - 1) + new string('#', n - (i - 1)));
+            }
+        }
+    }
+}
