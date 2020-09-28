f954514fab0b1890dd85c031c03ac30832a9b74b
Georgi <alienguymc@gmail.com>
2020-09-28 23:43:07 +0300

    add

---
+++ 01 - [Easy]/09 - [Time Conversion]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            bool success = DateTime.TryParse(input, out DateTime time);
+
+            if (success)
+            {
+                Console.WriteLine(time.ToString("HH:mm:ss"));
+            }
+        }
+    }
+}
