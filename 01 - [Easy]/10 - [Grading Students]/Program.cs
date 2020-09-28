818a558c50a7628870b35b4304b931b6653836f7
Georgi <alienguymc@gmail.com>
2020-09-29 00:04:18 +0300

    add

---
+++ 01 - [Easy]/10 - [Grading Students]/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+
+namespace GradingStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int grade = int.Parse(Console.ReadLine());
+
+                if (grade < 38)
+                {
+                    Console.WriteLine(grade);
+                }
+                else
+                {
+                    int multipleOfFive = ((grade + 4) / 5) * 5;
+
+                    Console.WriteLine(multipleOfFive - grade < 3 ? ((grade + 4) / 5) * 5 : grade);
+                }
+            }
+        }
+    }
+}
