4fdf74500a334da4a761ec067db3a2f1f686c297
Georgi <alienguymc@gmail.com>
2020-09-28 23:00:12 +0300

    add

---
+++ 01 - [Easy]/08 - [Birthday Cake Candles]/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var candles = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Take(n)
+                .ToList();
+
+            int equalCandlesCount = 0;
+            int maxCandle = candles.Max();
+
+            for (int i = 0; i < candles.Count; i++)
+            {
+                if (maxCandle == candles[i])
+                {
+                    equalCandlesCount++;
+                }
+            }
+
+            Console.WriteLine(equalCandlesCount);
+        }
+    }
+}
