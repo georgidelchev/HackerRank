21a40413d884a670f289e497b231519aadb31014
Georgi <alienguymc@gmail.com>
2020-09-28 22:49:34 +0300

    add

---
+++ 01 - [Easy]/07 - [Mini-Max Sum]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console
+                .ReadLine()
+                .Split()
+                .Select(long.Parse)
+                .ToList();
+
+            Console.WriteLine($"{arr.Sum() - arr.Max()} {arr.Sum() - arr.Min()}");
+        }
+    }
+}
