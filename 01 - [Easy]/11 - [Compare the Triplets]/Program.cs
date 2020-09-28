be78c9ce4ec5cd53e6950c75f5162fb2a84f18e5
Georgi <alienguymc@gmail.com>
2020-09-29 00:17:09 +0300

    add

---
+++ 01 - [Easy]/11 - [Compare the Triplets]/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+
+namespace CompareTheTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alicePoints = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var bobPoints = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int aliceTotalPoints = 0;
+            int bobTotalPoints = 0;
+
+            for (int i = 0; i < alicePoints.Count; i++)
+            {
+                int comparing = alicePoints[i].CompareTo(bobPoints[i]);
+
+                if (comparing > 0)
+                {
+                    aliceTotalPoints++;
+                }
+                else if (comparing < 0)
+                {
+                    bobTotalPoints++;
+                }
+            }
+
+            Console.WriteLine($"{aliceTotalPoints} {bobTotalPoints}");
+        }
+    }
+}
