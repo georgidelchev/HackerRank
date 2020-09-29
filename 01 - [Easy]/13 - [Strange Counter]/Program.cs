8c1e88090bc1dbb331b79fa06ddfcd13f8bb5023
Georgi <alienguymc@gmail.com>
2020-09-29 19:37:53 +0300

    add

---
+++ 01 - [Easy]/13 - [Strange Counter]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace StrangeCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long time = long.Parse(Console.ReadLine());
+            long startNumber = 3;
+
+            while (time > startNumber)
+            {
+                time -= startNumber;
+                startNumber *= 2;
+            }
+
+            Console.WriteLine(startNumber - time + 1);
+        }
+    }
+}
