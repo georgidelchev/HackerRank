dd53a85dd3d7b0525d6dec9cbe5b37fc1db9f01a
Georgi <alienguymc@gmail.com>
2020-10-01 00:03:43 +0300

    add

---
+++ 01 - [Easy]/19 - [Migratory Birds]/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MigratoryBirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            var birds = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var groups = birds
+                .GroupBy(x => x)
+                .OrderByDescending(x => x.Count())
+                .ThenBy(x => x.Key)
+                .FirstOrDefault();
+
+            Console.WriteLine(groups.Key);
+        }
+    }
+}
