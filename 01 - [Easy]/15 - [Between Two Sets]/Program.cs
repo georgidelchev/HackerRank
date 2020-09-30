e41914c5b8ef71de6fbd72b476fb3d9cd2e20f88
Georgi <alienguymc@gmail.com>
2020-09-30 17:34:33 +0300

    add

---
+++ 01 - [Easy]/15 - [Between Two Sets]/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace BetweenTwoSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numsCount = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int m = numsCount[0];
+            int n = numsCount[1];
+
+            var firstSequence = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Take(m)
+                .ToList();
+
+            var secondSequence = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Take(n)
+                .ToList();
+
+            int valids = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (firstSequence.TrueForAll(x => i % x == 0))
+                {
+                    if (secondSequence.TrueForAll(x => x % i == 0))
+                    {
+                        valids++;
+                    }
+                }
+            }
+
+            Console.WriteLine(valids);
+        }
+    }
+}
