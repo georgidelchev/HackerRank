7ac3de3aca86d8c552afc6071d916dc10f472f88
Georgi <alienguymc@gmail.com>
2020-09-30 17:48:59 +0300

    add

---
+++ 01 - [Easy]/16 - [Breaking the Records]/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace BreakingTheRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gamesCount = int.Parse(Console.ReadLine());
+
+            var scores = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Take(gamesCount)
+                .ToList();
+
+            int highestScoreBrokes = 0;
+            int lowestScoreBrokes = 0;
+
+            int min = scores[0];
+            int max = scores[0];
+
+            for (int i = 0; i < scores.Count; i++)
+            {
+                if (scores[i] > max)
+                {
+                    highestScoreBrokes++;
+                    max = scores[i];
+                }
+                else if (scores[i] < min)
+                {
+                    lowestScoreBrokes++;
+                    min = scores[i];
+                }
+            }
+
+            Console.WriteLine($"{highestScoreBrokes} {lowestScoreBrokes}");
+        }
+    }
+}
