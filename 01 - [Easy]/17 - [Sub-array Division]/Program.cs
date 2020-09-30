8a3cd4f14068d28035dfa858a626666011723a0d
Georgi <alienguymc@gmail.com>
2020-09-30 19:53:20 +0300

    add

---
+++ 01 - [Easy]/17 - [Sub-array Division]/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace SubArrayDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var chocoBars = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Take(n)
+                .ToList();
+
+            var birthDayAndMonth = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int d = birthDayAndMonth[0];
+            int m = birthDayAndMonth[1];
+
+            int waysToSplitCounter = 0;
+
+            for (int i = 0; i < (chocoBars.Count - m) + 1; i++)
+            {
+                var sum = chocoBars
+                    .GetRange(i, m)
+                    .Aggregate((x, y) => x + y);
+
+                if (sum == d)
+                {
+                    waysToSplitCounter++;
+                }
+            }
+
+            Console.WriteLine(waysToSplitCounter);
+        }
+    }
+}
