b9320c814b8947ad88a08d3983292d8a494a5cf6
Georgi <alienguymc@gmail.com>
2020-09-30 20:17:48 +0300

    add

---
+++ 01 - [Easy]/18 - [Divisible Sum Pairs]/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace DivisibleSumPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int size = input[0];
+            int divisibleNumber = input[1];
+
+            var arr = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Take(size)
+                .ToList();
+
+            int divisiblesCount = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                for (int j = i + 1; j < arr.Count; j++)
+                {
+                    if ((arr[i] + arr[j]) % divisibleNumber == 0)
+                    {
+                        divisiblesCount++;
+                    }
+                }
+
+            }
+
+            Console.WriteLine(divisiblesCount);
+        }
+    }
+}
