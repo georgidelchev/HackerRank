4cf36b8c9cb90e8d574bf956fad21ee3fd401313
Georgi <alienguymc@gmail.com>
2020-10-05 21:57:35 +0300

    add

---
+++ 01 - [Easy]/22 - [Drawing Book]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DrawingBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pagesCount = int.Parse(Console.ReadLine());
+            int pageToTurnTo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Min(pageToTurnTo / 2, pagesCount / 2 - pageToTurnTo / 2));
+        }
+    }
+}
