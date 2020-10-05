db8bfa8953b0e51c1d9f737bbbe4a321eb4b6896
Georgi <alienguymc@gmail.com>
2020-10-05 21:36:13 +0300

    add

---
+++ 01 - [Easy]/21 - [Sales by Match]/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesByMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var nums = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Take(n)
+                .ToList();
+
+            var dict = new Dictionary<int, int>();
+
+            int counter = 0;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict[nums[i]] = 0;
+                }
+
+                dict[nums[i]]++;
+
+                if (dict[nums[i]] == 2)
+                {
+                    dict[nums[i]] = 0;
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
