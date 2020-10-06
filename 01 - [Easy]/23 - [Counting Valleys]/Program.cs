f18e68aab920b82148c7e85089a7ae94fe7e3951
Georgi <alienguymc@gmail.com>
2020-10-06 09:37:55 +0300

    add

---
+++ 01 - [Easy]/23 - [Counting Valleys]/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var moves = Console
+                .ReadLine()
+                .ToCharArray();
+
+            int valleysCount = 0;
+            int seaLvl = 0;
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                if (moves[i] == 'D')
+                {
+                    seaLvl--;
+                }
+                else
+                {
+                    seaLvl++;
+                }
+
+                if (moves[i] == 'U' && seaLvl == 0)
+                {
+                    valleysCount++;
+                }
+            }
+
+            Console.WriteLine(valleysCount);
+        }
+    }
+}
