31a7d0522f00fbafd89ba30929a172f1c57674a8
Georgi <alienguymc@gmail.com>
2020-10-07 11:22:26 +0300

    add

---
+++ 01 - [Easy]/25 - [Viral Advertising]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ViralAdvertising
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+
+            double shares = 5;
+            double totalLikes = 0;
+
+            for (int i = 0; i < days; i++)
+            {
+                shares = Math.Floor(shares / 2);
+                totalLikes += shares;
+                shares *= 3;
+            }
+            Console.WriteLine(totalLikes);
+        }
+    }
+}
