3b583e6c1ea6f4ba362c72e8c5453c2efae88821
georgidelchev <alienguymc@gmail.com>
2020-10-08 21:15:35 +0300

    add

---
+++ 01 - [Easy]/26 - [Maximum Perimeter Triangle]/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumPerimeterTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> intParseFunc = x => int.Parse(x);
+
+            int size = int.Parse(Console.ReadLine());
+
+            var nums = Console
+                .ReadLine()
+                .Split()
+                .Select(intParseFunc)
+                .OrderBy(x => x)
+                .Take(size)
+                .ToList();
+
+            var max = new List<int>();
+            var maxSum = int.MinValue;
+
+            for (int i = 0; i < nums.Count - 2; i++)
+            {
+                int firstNumber = nums[i];
+                int secondNumber = nums[i + 1];
+                int thirdNumber = nums[i + 2];
+
+                if (firstNumber + secondNumber > thirdNumber)
+                {
+                    if (firstNumber + secondNumber + thirdNumber > maxSum)
+                    {
+                        maxSum = firstNumber + secondNumber + thirdNumber;
+
+                        max.Clear();
+                        max.Add(firstNumber);
+                        max.Add(secondNumber);
+                        max.Add(thirdNumber);
+                    }
+                }
+            }
+
+            if (max.Any())
+            {
+                Console.WriteLine(string.Join(" ", max));
+
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
