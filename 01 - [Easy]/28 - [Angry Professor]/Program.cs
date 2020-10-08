2b1960859c1ffe4d41ca6409a07fb1f267404ae6
georgidelchev <alienguymc@gmail.com>
2020-10-08 23:59:59 +0300

    add

---
+++ 01 - [Easy]/28 - [Angry Professor]/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AngryProfessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testCases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testCases; i++)
+            {
+                var input = Console
+                    .ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToList();
+
+                var studsCount = input[0];
+                var cancellationThreshold = input[1];
+
+                var arrivalTimes = Console
+                    .ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .Where(x => x <= 0)
+                    .ToList();
+
+                if (arrivalTimes.Count >= cancellationThreshold)
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("YES");
+                }
+            }
+        }
+    }
+}
