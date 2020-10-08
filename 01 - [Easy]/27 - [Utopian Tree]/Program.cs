9990ca057eee3b8556398ad71987abb306f253e0
georgidelchev <alienguymc@gmail.com>
2020-10-08 23:43:55 +0300

    add

---
+++ 01 - [Easy]/27 - [Utopian Tree]/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UtopianTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int testCase = int.Parse(Console.ReadLine());
+
+                int height = 1;
+                for (int j = 1; j < testCase + 1; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        height *= 2;
+                    }
+                    else
+                    {
+                        height++;
+                    }
+                }
+
+                Console.WriteLine(height);
+            }
+
+        }
+    }
+}
