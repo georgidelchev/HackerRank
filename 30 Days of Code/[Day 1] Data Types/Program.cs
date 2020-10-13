cec7b72fdba9d269820bc5b3dc967cb9f59fcffd
Georgi <alienguymc@gmail.com>
2020-10-13 11:32:54 +0300

    add

---
+++ 30 Days of Code/[Day 1] Data Types/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank";
+
+            int i2 = int.Parse(Console.ReadLine());
+            double d2 = double.Parse(Console.ReadLine());
+            string s2 = Console.ReadLine();
+
+            Console.WriteLine(i + i2);
+            Console.WriteLine($"{d + d2:f1}");
+            Console.WriteLine(s + " " + s2);
+        }
+    }
+}
